917f3e32450513035504aa64267285df15fd4090
mengxl8 <mengxl8@lenovo.com>
2023-07-14 16:45:06 +0800

    使用mantine官方代码切换暗黑模式

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,49 @@
 import "./App.css";
 
-import { Header, MantineProvider } from "@mantine/core";
+import {
+  Header,
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+} from "@mantine/core";
 import { ActionIcon } from "@mantine/core";
 import { IconSunHigh, IconMoon } from "@tabler/icons-react";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 export default function App() {
-  const [theme, setTheme] = useState({ colorScheme: "dark" });
-
-  const toggleTheme = () => {
-    if (theme.colorScheme === "dark") {
-      setTheme({ colorScheme: "light" });
-    } else {
-      setTheme({ colorScheme: "dark" });
-    }
-  };
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
 
   return (
-    <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
-      <Header height={60} p="xs">
-        <div className="flex justify-between items-center">
-          <span>dhhh</span>
-          <ActionIcon
-            onClick={toggleTheme}
-            size="lg"
-            radius="md"
-            variant="default"
-          >
-            {theme.colorScheme === "dark" ? <IconMoon /> : <IconSunHigh />}
-          </ActionIcon>
-        </div>
-      </Header>
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <Header height={60} p="xs">
+          <div className="flex justify-between items-center">
+            <span>得好好混的小破站</span>
+            <ActionIcon
+              onClick={() => toggleColorScheme()}
+              size="lg"
+              radius="md"
+              variant="default"
+            >
+              {colorScheme === "dark" ? <IconMoon /> : <IconSunHigh />}
+            </ActionIcon>
+          </div>
+        </Header>
 
-      <div className="home min-w-[1100px]">
-        <Outlet />
-      </div>
-    </MantineProvider>
+        <div className="home min-w-[1100px]">
+          <Outlet />
+        </div>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
